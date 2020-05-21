34700dcd1b5a8089358d392589ccd2e506b0d848
nagendranayak <63073234+nagendranayak@users.noreply.github.com>
2020-05-21 11:39:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
 
 
 
